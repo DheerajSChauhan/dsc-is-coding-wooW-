d851cbe8f07f7dd0fde878840d104a2c4f29feda
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-01-07 12:30:29 +0530

    Time: 0 ms (100%), Space: 26.2 MB (32.93%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,17 +1,55 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans= 0;
-        int n = nums.size(); 
-        for( int x: nums){
-            ans = ans ^ x;
-        }
+        
+        //brute force aproach : using XOR operation.
+        // int ans= 0;
+        // int n = nums.size(); 
 
+        // using for each loop:
 
+        // for( int x: nums){
+        //     ans = ans ^ x;
+        // }
+
+        // using for loop:
         // for( int i = 0 ;i<n; i++){
         //     ans = ans^nums[i];
         // }
-        return ans;        
+        //return ans;
+
+
+        //optimal apporach : using Binaray Search:
+        int n = nums.size();
+        int l = 0;
+        int h = n-1;
+        while( l < h){
+            int mid = l + (h - l)/2;
+
+            //rightside even hai ya odd nikal lo
+            bool isEven;
+            if((h - mid)%2 ==0){
+                isEven = true;
+            }else{
+                isEven = false;
+            }
+
+            //check karo bagal wala element equall hai ya nahi
+            if( nums[mid] == nums[mid + 1]){
+                if(isEven){ // for even
+                    l = mid+2;
+                }else{ // for odd
+                    h= mid-1;
+                }
+            }else{ // nums[mid] != nums[mid+1]
+                if( isEven){
+                    h= mid;
+                }else{ // for odd
+                    l = mid+1;
+                }
+            }
+        }
+        return nums[h];
     }
 
 };
