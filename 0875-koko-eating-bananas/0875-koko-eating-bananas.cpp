12a456265acd1965a8be3b0ad4afc7335594eb5b
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-01-09 13:24:59 +0530

    Time: 8 ms (56.57%), Space: 22.9 MB (76.21%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canEatAll(vector<int>& piles, int mid, int h){
+        int actualHours = 0;
+        for(int x : piles){
+            actualHours = actualHours + x/mid; //hours nikal jayega
+
+            if( x%mid != 0){
+                actualHours++;
+            }
+        }
+        return actualHours <= h;
+    }
+
+
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(begin(piles), end(piles));
+
+        while( l < r){
+            int mid= l+(r-l)/2;
+
+            if( canEatAll(piles, mid, h)){
+                r = mid;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
