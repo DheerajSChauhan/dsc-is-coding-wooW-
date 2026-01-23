f3abc5baada9aac0d7fc282283a4753c015b0414
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-01-23 12:07:05 +0530

    Time: 0 ms (100%), Space: 9 MB (53.47%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        int depth = 0;
+        for( auto ch: s){
+            if( ch == '('){
+                if(depth > 0) ans+= ch;
+                depth++;
+            }else{
+                depth--;
+                if(depth >0) ans+= ch;
+            }
+        }
+        return ans;
+    }
+};
