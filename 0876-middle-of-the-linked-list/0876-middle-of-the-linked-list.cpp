aefdc7832dcf0e19aba7e4bcb67a4076fdfaf5a8
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-01-28 12:29:55 +0530

    Time: 0 ms (100%), Space: 10 MB (62.35%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        //Optimal Solution
+        // ListNode* fast = head;
+        // ListNode* slow = head;
+        // while(fast != nullptr && fast->next != nullptr){
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        // }
+        // return slow;
+
+        //Brute Force Solution
+        // int cnt = 0;
+        // ListNode* temp = head;
+        // while( temp != nullptr){
+        //     cnt++;
+        //     temp= temp->next;
+        // }
+        // int midNode = cnt/2 + 1;
+        // temp = head;
+        // while(temp != nullptr){
+        //     midNode = midNode-1;
+        //     if(midNode == 0){
+        //         break;
+        //     }
+        //     temp= temp->next;
+        // }
+        // return temp;
+
+       // using slow/fast pointer traversal
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while( fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+        return slow;
+    
+    }
+};
