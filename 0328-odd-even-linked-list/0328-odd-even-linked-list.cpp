a600caeced6000917a0ff5e01e9dab082daba438
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-01-28 13:19:50 +0530

    Time: 0 ms (100%), Space: 15.6 MB (26.96%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,20 +11,25 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        if( head == nullptr || head->next == nullptr){
+            return head;
+        }
 
-        if(head == nullptr || head->next == nullptr) return head;
         ListNode* odd = head;
         ListNode* even = head->next;
-        ListNode* evenHead = even;
 
-        while(even != nullptr && even->next != nullptr){
-            odd->next = odd->next->next;
+        ListNode* evenStart = head->next;
+
+        while( even != nullptr && even->next != nullptr){
+            odd->next = even->next;
             even->next = even->next->next;
 
             odd = odd->next;
             even = even->next;
-        }
-        odd->next = evenHead;
+        }   
+        //aub odd ko even se connect karna hai
+        odd->next = evenStart;
+
         return head;
     }
 };
