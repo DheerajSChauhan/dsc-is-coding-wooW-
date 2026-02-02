9a24c510eaf5ab4acceec90ce99b286dc8329fed
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-02-02 11:24:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    
-    double power( double x, long long n){
-        //base case
-        if(n == 0)return 1;
-
-        //recursion call(aadhi power)
-        double half = power(x, n/2);
-
-        //if even n
-        if( n % 2 == 0){
-            return half* half;
-        }else{
-            ///odd n hai
-            return x * half* half;
-        }
-
+    double solve( double x, long long n){
+        if( n == 0 )
+            return 1;
+        if( n < 0)
+            return solve(1/x, -n);
+        if( n % 2 == 0)
+            return solve( x*x, n/2);
+        return x* solve( x*x, n/2);
     }
-
-
     double myPow(double x, int n) {
-        long long N = n; //overflow
-
-        if( N < 0){
-            x = 1/x;
-            N = -N;
-        }
-        return power(x,N);
+        return solve( x, (long) n);
     }
 };
