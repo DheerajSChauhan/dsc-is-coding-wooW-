5c7250a993bb00b93d26b9b744bbdd22e4b3128e
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-02-03 10:51:01 +0530

    Time: 0 ms (100%), Space: 16.9 MB (10.06%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    bool isValid(string str){
-        int count = 0;
-        for( char &ch : str){
-            if( ch == '('){
-                count++;
-            }else{
-                count--;
-            }
-            if( count < 0){
-                return false;
-            }
-        }
-        return count == 0;
-    }
+    // bool isValid(string str){
+    //     int count = 0;
+    //     for( char &ch : str){
+    //         if( ch == '('){
+    //             count++;
+    //         }else{
+    //             count--;
+    //         }
+    //         if( count < 0){
+    //             return false;
+    //         }
+    //     }
+    //     return count == 0;
+    // }
 
     vector<string>result;
     
-    void solve(string curr, int n){
+    void solve(string curr, int n, int open, int close){
         if(curr.length() == 2*n){
-            if( isValid(curr)){
-                result.push_back(curr);
-            }
+            result.push_back(curr);
             return;
         }
-        curr.push_back('(');
-        solve(curr, n);
-        curr.pop_back();
+        if( open < n){
+            curr.push_back('(');
+            solve(curr, n, open +1, close);
+            curr.pop_back();
 
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
+        }
+        if( close < open){
+            curr.push_back(')');
+            solve(curr, n, open, close + 1);
+            curr.pop_back();
+        }
+        
     }
     vector<string> generateParenthesis(int n) {
         string curr = "";
-        solve(curr, n);
+
+        int open = 0;
+        int close = 0;
+        solve(curr, n, open, close);
         return result;
     }
 };
