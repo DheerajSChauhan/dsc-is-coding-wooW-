3905f33d664c3be10022a9729c361c9b2cef3f43
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-03-11 11:23:00 +0530

    Time: 0 ms (100%), Space: 13 MB (13.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        //brute force apporoach
-        vector<int>ans;
+        /*
+        1. Traverse from right to left
+        2. mantain the stack of grater element
+        3. pop all the smaller element 
+        4. stack top = next greater element
+        5.store in map;
+        */
 
-        //nums1 ke har element ke liye
-        for(int i=0; i<nums1.size(); i++){
-            int element = nums1[i];
-            int index = -1;
-            //nums2 me element ke position find karo
-            for( int j=0; j< nums2.size(); j++){
-                if(nums2[j] == element){
-                    index = j;
-                    break;
-                }
+        stack<int>st; //stack to maintain the decreasing order
+        unordered_map<int, int>mp; //map->element->next greater element;
+        //nums2 me right to left traverse kar lete hai
+        for(int i=nums2.size()-1; i>=0; i--){
+            int curr = nums2[i];
+            while(!st.empty() && st.top() <= curr){
+                st.pop();
             }
-            int nextGreater = -1;
-            //nums2 ke right side me next greater element find karo
-            for( int i=index+1; i<nums2.size(); i++){
-                if(element < nums2[i]){
-                    nextGreater = nums2[i];
-                    break;
-                }
+            //if stack empty ho gaya to -1 map me dal do
+            if( st.empty()){
+                mp[curr] = -1;
+            }else{
+                //agar ye hai toh ye next greater eement he hoga
+                mp[curr] = st.top();
             }
-            ans.push_back(nextGreater);
+            //push it to the stack
+            st.push(curr);
+        }
+        vector<int>ans;
+        //nums1 ke answers map se fetch karo
+        for( int i= 0; i<nums1.size(); i++){
+            ans.push_back(mp[nums1[i]]);
         }
         return ans;
     }
