7d0556e02fa84ab88ce2e352a809afa3b2b4a3eb
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-04-09 20:00:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.94%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.size();
-        string ans= "";
-        int i = n-1;
 
-        while( i >= 0){ //right to left move kar rahe hai (reverse string chaiye)
-            //step 1: extra spaces ko hatta do
-            while( i>=0 && s[i] == ' ') i--;
-            //step 2: check kar lo kahhe string poora space toh nahi
-            if( i<0)break; //agar poore string space hai toh break kar do
-
-            //step 3: word k last index mark karo
-            int j=i;
-            //step:5; word ka start doodho
-            while( i >=0 && s[i] != ' ') i--;
-            //step 5: agar aub sapce aaya matlab word k beech space ko add karo
-            if( !ans.empty()) ans += ' ';
-            //step 6: word extrac karo
-            ans += s.substr(i+1, j-i); 
+        reverse(s.begin(), s.end());
+        //using two pointer
+        int l = 0, r = 0, i = 0;
+        int n= s.length();
+        while( i < n){
+            while(i<n && s[i] == ' '){
+                i++;
+            }
+            while( i<n && s[i] != ' '){
+                s[r++] = s[i++]; 
+            }
+            if(l<r){
+                reverse(s.begin()+l, s.begin()+r);
+                s[r++] = ' ';
+                l=r;
+            }
         }
-        return ans;
+        return s.substr(0, r-1);
     }
 };
