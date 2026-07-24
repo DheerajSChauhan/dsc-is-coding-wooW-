94046937265a435bdd6a85a8704c9b5899c3019c
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-07-24 18:25:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -13,30 +13,58 @@ public:
             adj[v].push_back({u, wt});
         }
 
-        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
-
+        //priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        set<pair<int, int>>st;
         vector<int> result(V, INT_MAX);
 
         result[src] = 0;
-        pq.push({0, src});
+        //pq.push({0, src});
+        st.insert({0, src});
+        
+        //using priority queue;
+        // while (!pq.empty()) {
 
-        while (!pq.empty()) {
+        //     int d = pq.top().first;
+        //     int node = pq.top().second;
+        //     pq.pop();
 
-            int d = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+        //     if (d > result[node])
+        //         continue;
 
-            if (d > result[node])
-                continue;
+        //     for (auto &vec : adj[node]) {
+
+        //         int adjNode = vec.first;
+        //         int wt = vec.second;
+
+        //         if (d + wt < result[adjNode]) {
+        //             result[adjNode] = d + wt;
+        //             pq.push({result[adjNode], adjNode});
+        //         }
+        //     }
+        // }
+        
+        //using set;
+        while (!st.empty()) {
+            
+            auto &it = *st.begin();
+            int d = it.first;
+            int node = it.second;
+            st.erase(it);
+
 
             for (auto &vec : adj[node]) {
 
                 int adjNode = vec.first;
                 int wt = vec.second;
-
-                if (d + wt < result[adjNode]) {
-                    result[adjNode] = d + wt;
-                    pq.push({result[adjNode], adjNode});
+                
+                //remove old enrty if exists;
+                if(d + wt < result[adjNode]){
+                    if(result[adjNode] != INT_MAX){
+                        st.erase({result[adjNode], adjNode});
+                    }
+                    
+                    result[adjNode] = d+wt;
+                    st.insert({result[adjNode], adjNode});
                 }
             }
         }
