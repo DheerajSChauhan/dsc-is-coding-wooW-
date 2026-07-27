0305f12f3d5665942b6d6b44aa5c212f97452573
Dheeraj Singh Chauhan <115028559+DheerajSChauhan@users.noreply.github.com>
2026-07-27 13:01:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int>result(V, 1e8);
+        result[src] = 0;
+        
+        for(int count = 1; count <= V-1; count++){
+            for(auto &edge: edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(result[u] != 1e8 && result[u] + w < result[v]){
+                    result[v] = result[u] + w;
+                }
+            }
+        }
+        
+        //now detect the cycle;
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            
+            if(result[u] != 1e8 && result[u] + w < result[v]){
+                return {-1};
+            }
+        }
+        
+        return result;
+    }
+};
